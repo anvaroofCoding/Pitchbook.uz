4e2349b0ee870dd529fc98a6ab60b40a59bbbb6d
Islom <islom@Isloms-MacBook-Air.local>
2025-06-03 20:44:37 +0500

    main update border unders

--- src/components/BackgroundBeans.jsx
+++ src/components/BackgroundBeans.jsx
@@ -6,7 +6,7 @@ export function BackgroundBeamsWithCollisionDemo() {
 	const t = useTranslations('xususiyatlar')
 	return (
 		<BackgroundBeamsWithCollision className=' h-[200px] lg:h-[300px]'>
-			<h2 className='text-2xl relative z-20 md:text-4xl border lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight'>
+			<h2 className='text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight'>
 				{t('main_gallery')}?
 				<div className='relative ml-5 mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]'>
 					<div className='absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]'>
